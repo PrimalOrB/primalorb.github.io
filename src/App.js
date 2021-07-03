c2b2e5f6b61b1679fb291ad2213d9425a2c15d11
Andrew Ogilvie <primalorb@gmail.com>
2021-07-03 15:38:22 -0400

    responsive about

--- src/App.js
+++ src/App.js
@@ -19,27 +19,29 @@ function App() {
     return (
         <div className="page">
             <Background />
-            <Header currentPage={ currentPage } allPages={ allPages } setCurrentPage={ setCurrentPage }/>
-            <section className="content-wrapper">
-                <Switch>
-                    <Route exact path="/"> 
-                        <About />
-                    </Route>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                    <Route path="/work">
-                        <Work />
-                    </Route>
-                    <Route path="/contact">
-                        <Contact />
-                    </Route>
-                    <Route path="/resume">
-                        <Resume />
-                    </Route>
-                </Switch>
+            <section className="container">
+                <Header currentPage={ currentPage } allPages={ allPages } setCurrentPage={ setCurrentPage }/>
+                <section className="content-wrapper">
+                    <Switch>
+                        <Route exact path="/"> 
+                            <About />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/work">
+                            <Work />
+                        </Route>
+                        <Route path="/contact">
+                            <Contact />
+                        </Route>
+                        <Route path="/resume">
+                            <Resume />
+                        </Route>
+                    </Switch>
+                </section>
+                <Footer />
             </section>
-            <Footer />
         </div>
     )
 }
